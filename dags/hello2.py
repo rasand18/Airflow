7cee0bfa9ebadc2d69f1b5b51c619e7dba71002e
Rasmus Andreasson <144208740+RA5673@users.noreply.github.com>
2024-10-24 16:35:21 +0200

    test

--- dags/hello2.py
+++ dags/hello2.py
@@ -7,3 +7,19 @@ default_args = {
     'start_date': datetime(2024, 10, 20),
     'catchup': False
 }
+
+dag = DAG(
+    'check_directory',
+    default_args=default_args,
+    schedule=timedelta(days=1)
+)
+
+# Kontrollera om katalogen finns
+t1 = BashOperator(
+    task_id='check_directory_exists',
+    bash_command='[ -d /opt/airflow/dags/repo/dags ] && echo "Directory exists" || echo "Directory does not exist"',
+    queue='kubernetes',
+    dag=dag
+)
+
+t1

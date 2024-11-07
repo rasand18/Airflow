76a4f3ff11891cefe73b5ab54e44f35182373b99
Rasmus Andreasson <144208740+RA5673@users.noreply.github.com>
2024-11-07 09:22:49 +0100

    test

--- dags/hello2.py
+++ dags/hello2.py
@@ -2,6 +2,8 @@ from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.utils.task_group import TaskGroup
 from airflow.operators.python_operator import PythonOperator
 from airflow.hooks.base_hook import BaseHook
 import boto3
@@ -57,34 +59,46 @@ dag = DAG(
 
 # Skapa parallella Spark tasks och sensorer
 num_tasks = 10  # Antalet parallella instanser
-spark_tasks = []
-sensor_tasks = []
 
-for i in range(num_tasks):
-    spark_task = SparkKubernetesOperator(
-        task_id=f'n-spark-on-k8s-airflow-{i+1}',
-        trigger_rule="all_success",
-        depends_on_past=False,
-        retries=0,
-        application_file='spark-pi.yaml',
-        namespace="spark-operator",
-        kubernetes_conn_id="spark-k8s",
-        do_xcom_push=True,
-        dag=dag
-    )
+with TaskGroup("spark_tasks_group", dag=dag) as spark_tasks_group:
+    spark_tasks = []
+    sensor_tasks = []
+    
+    for i in range(num_tasks):
+        spark_task = SparkKubernetesOperator(
+            task_id=f'n-spark-on-k8s-airflow-{i+1}',
+            trigger_rule="all_success",
+            depends_on_past=False,
+            retries=0,
+            application_file='spark-pi.yaml',
+            namespace="spark-operator",
+            kubernetes_conn_id="spark-k8s",
+            do_xcom_push=True,
+            dag=dag
+        )
 
-    sensor_task = SparkKubernetesSensor(
-        task_id=f'spark_pi_monitor_{i+1}',
-        namespace="spark-operator",
-        application_name="{{ task_instance.xcom_pull(task_ids='n-spark-on-k8s-airflow-" + str(i + 1) + "')['metadata']['name'] }}",
-        kubernetes_conn_id="spark-k8s",
-        dag=dag,
-        attach_log=True
-    )
+        sensor_task = SparkKubernetesSensor(
+            task_id=f'spark_pi_monitor_{i+1}',
+            namespace="spark-operator",
+            application_name="{{ task_instance.xcom_pull(task_ids='n-spark-on-k8s-airflow-" + str(i + 1) + "')['metadata']['name'] }}",
+            kubernetes_conn_id="spark-k8s",
+            dag=dag,
+            attach_log=True
+        )
 
-    spark_task >> sensor_task  # Definiera beroendet mellan task och sensor
-    spark_tasks.append(spark_task)
-    sensor_tasks.append(sensor_task)
+        cancel_task = KubernetesPodOperator(
+            task_id=f"cancel_spark_job_{i+1}",
+            namespace="default",
+            name=f"cancel-spark-job-{i+1}",
+            image="bitnami/kubectl:latest",
+            cmds=["kubectl", "delete", "sparkapplication", "{{ task_instance.xcom_pull(task_ids='n-spark-on-k8s-airflow-" + str(i + 1) + "')['metadata']['name'] }}"],
+            dag=dag,
+            trigger_rule="all_done"  # Körs endast om den manuellt triggas
+        )
 
-# Kör alla Spark tasks parallellt
-spark_tasks
+        # Definiera beroenden
+        spark_task >> sensor_task  # Spark-jobb följs av sensor
+        sensor_task >> cancel_task  # Lägg cancel som sista steg i TaskGroup
+
+        spark_tasks.append(spark_task)
+        sensor_tasks.append(sensor_task)

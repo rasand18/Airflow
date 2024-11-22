eea417ad4c69d68d2e2187ed52edb5d886afb7b8
Rasmus Andreasson <144208740+RA5673@users.noreply.github.com>
2024-11-22 11:09:23 +0100

    test

--- dags/hello4.py
+++ dags/hello4.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from datetime import datetime
 
 default_args = {
@@ -10,7 +10,13 @@ default_args = {
 
 with DAG('dbt_test', default_args=default_args, schedule_interval=None) as dag:
 
-    dbt_debug = BashOperator(
+    dbt_debug = KubernetesPodOperator(
         task_id='dbt_debug',
-        bash_command='dbt debug'
+        namespace='default',  # Namespace där podden körs
+        image='harbor.ad.spendrups.se/dataplatform-test/dbt:1.0',
+        cmds=["sh", "-c"],
+        arguments=["dbt debug"],
+        name="dbt-debug-pod",
+        is_delete_operator_pod=True,  # Ta bort podden efter körning
+        get_logs=True  # Hämta loggar från podden
     )

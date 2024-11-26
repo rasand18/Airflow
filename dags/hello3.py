cce782f27e3395e247ece3d36c81e122f1ee0c25
Rasmus Andreasson <144208740+RA5673@users.noreply.github.com>
2024-11-26 10:16:21 +0100

    test

--- dags/hello3.py
+++ dags/hello3.py
@@ -3,38 +3,43 @@ from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 
+# Lista över tabeller
+TABLES = ["CSYTAB_clean_system_settings"]
+
 default_args = {
     'owner': 'datamasterylab.com',
     'start_date': datetime(2024, 10, 31),
     'catchup': False
 }
 
-# Skapa en enkel DAG
+# Skapa DAG
 with DAG(
-    'simple_spark_dag',
+    dag_id="dynamic_spark_table_dag",
     default_args=default_args,
     schedule=timedelta(days=1),
     template_searchpath='/opt/airflow/dags/repo/dags/',
     catchup=False
 ) as dag:
 
-    # Spark Kubernetes Operator för att köra Spark-jobbet
-    spark_task = SparkKubernetesOperator(
-        task_id='spark_on_k8s',
-        application_file='sparkTransformToAzure.yaml',  # Din Spark-applikationens specifikation
-        namespace="spark-operator",
-        kubernetes_conn_id="spark-k8s",
-        do_xcom_push=True
-    )
+    for table_name in TABLES:
+        # Spark Kubernetes Operator-task
+        spark_task = SparkKubernetesOperator(
+            task_id=f"spark_task_{table_name}",
+            namespace="spark-operator",
+            application_file="spark_application_template.yaml",  # Din Spark YAML-template
+            kubernetes_conn_id="spark-k8s",
+            do_xcom_push=True,
+            env_vars={"SPARK_INPUT_TABLE": table_name},  # Dynamiskt tabellnamn
+        )
 
-    # Sensor för att övervaka Spark-jobbet
-    sensor_task = SparkKubernetesSensor(
-        task_id='spark_pi_monitor',
-        namespace="spark-operator",
-        application_name="spark-python-app-k8s",
-        kubernetes_conn_id="spark-k8s",
-        attach_log=True
-    )
+        # Sensor för att övervaka Spark-jobbet
+        sensor_task = SparkKubernetesSensor(
+            task_id=f"monitor_{table_name}",
+            namespace="spark-operator",
+            application_name=f"spark-python-app-{table_name}",  # Matchar application_name i din YAML
+            kubernetes_conn_id="spark-k8s",
+            attach_log=True
+        )
 
-    # Definiera task-beroenden
-    spark_task >> sensor_task
+        # Definiera task-beroenden
+        spark_task >> sensor_task

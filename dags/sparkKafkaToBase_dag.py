85b21430fbedb0cd4b64edcc0fbf3c8f612ae95c
Rasmus Andreasson <144208740+RA5673@users.noreply.github.com>
2024-12-02 13:43:36 +0100

    test

--- dags/sparkKafkaToBase_dag.py
+++ dags/sparkKafkaToBase_dag.py
@@ -9,13 +9,13 @@ TABLES = [
     # {"table_name": "/m3/MVXCJDB.CSYTAB"},
     # {"table_name": "/m3/MVXCJDB.OOLINE"},
     # {"table_name": "/m3/MVXCJDB.OOHEAD"},
-    {"table_name": "/m3/MVXCJDB.ODHEAD"}
+    # {"table_name": "/m3/MVXCJDB.ODHEAD"}
     # {"table_name": "/m3/MVXCJDB.MITMAS"},
     # {"table_name": "/m3/MVXCJDB.MITHRY"},
     # {"table_name": "/m3/MVXCJDB.MITPOP"},
     # {"table_name": "/m3/MVXCJDB.MITISH"},
     # {"table_name": "/m3/MVXCJDB.OLICHA"}
-    # {"table_name": "/m3/MVXCJDB.CSYTAB", "queue_name": "medium", "driver_cores": 2, "driver_memory": "2G", "executor_instances": 3, "executor_cores": 2, "executor_memory": "2G"},
+    {"table_name": "/m3/MVXCJDB.ODHEAD", "queue_name": "medium", "driver_cores": 2, "driver_memory": "2G", "executor_instances": 3, "executor_cores": 2, "executor_memory": "2G"},
 ]
 
 default_args = {

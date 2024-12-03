e28caecd58cc4d7afd36459a23051534dfe75ea6
Rasmus Andreasson <144208740+RA5673@users.noreply.github.com>
2024-12-03 09:39:16 +0100

    test

--- dags/hello4.py
+++ dags/hello4.py
@@ -1,23 +1,37 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
-from cosmos.providers.dbt.core.operators.kubernetes import DbtKubernetesPodOperator
+from cosmos import DbtDag
 from datetime import datetime
+
+
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023, 1, 1),
+    'start_date': datetime(2024, 12, 2),
     'retries': 1
 }
 
-with DAG('dbt_test', default_args=default_args, schedule_interval=None) as dag:
-
-    dbt_debug = KubernetesPodOperator(
-        task_id='dbt_debug',
-        namespace='spark-operator',  # Namespace där podden körs
-        image='harbor.ad.spendrups.se/dataplatform-test/dbt:1.0',
-        cmds=["sh", "-c"],
-        arguments=["dbt deps && dbt run"],  # Kör både dbt deps och dbt run
-        name="dbt-debug-pod",
-        get_logs=True,  # Hämta loggar från podden
-        image_pull_policy='Always'  # Sätt pulling policy till Always
+# Skapa DAG
+with DAG(
+    dag_id='dbt_test',
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+    
+    # Skapa dbt DAG med Cosmos
+    dbt_dag = DbtDag(
+        dag_id="dbt",  # Unikt ID för DAG
+        dbt_project_dir="/dataplatform_spendrups_test",  # Path till ditt dbt-projekt
+        dbt_profiles_dir="/dataplatform_spendrups_test",  # Path till dbt-profiler
+        operator=KubernetesPodOperator,  # Kör dbt med KubernetesPodOperator
+        operator_args={
+            "namespace": "spark-operator",  # Namespace där KubernetesPod körs
+            "image": "harbor.ad.spendrups.se/dataplatform-test/dbt:1.0",  # Din dbt Docker-image
+            "cmds": ["sh", "-c"],  # Kör kommandon i sh
+            "arguments": ["dbt deps && dbt run"],  # dbt-kommandon
+            "get_logs": True,  # Hämta loggar från podden
+            "is_delete_operator_pod": True,  # Rensa pods efter körning
+            "image_pull_policy": "Always",  # Dra alltid senaste image
+        },
     )

06ecdd6c68ce758c05e66840b552253b0d38c463
Rasmus Andreasson <144208740+RA5673@users.noreply.github.com>
2024-12-03 10:43:15 +0100

    test

--- dags/hello4.py
+++ dags/hello4.py
@@ -18,7 +18,7 @@ with DAG('dbt_test', default_args=default_args, schedule_interval=None) as dag:
 
     # KubernetesPodOperator för att köra DBT-kommandon
     dbt_debug = KubernetesPodOperator(
-        task_id='dbt_debug',
+        task_id='dbt_base_to_transform',
         namespace='spark-operator',  # Namespace där podden körs
         image='harbor.ad.spendrups.se/dataplatform-test/dbt:1.0',
         cmds=["sh", "-c"],
